1ee59398454ebff96d8c2e812e0ba700dac5a061
lucasloughner <lucasloughner@comcast.net>
2023-02-08 20:41:49 -0800

    Updated Oracle to support wait times >2min

--- Oracle.py
+++ Oracle.py
@@ -13,15 +13,22 @@ def oracleFunc():
 
 def guessRN(rn):
     # Takes b64 encoded output of mersenne twister and tries to guess it
-    # Assumption: Test range of 2m05s to 10s before function is called
+    # Assumption: Test range of 2m05s to 10s before function is called, although function supports longer wait times
     # This range is chose because oracle func waits anywhere from 10s to 2min, add 5 sec to be safe
     t = int(time.time())
-    for i in range(t-125, t-10):
-        mt = MersenneTwister.MersenneTwister(i)
-        out = base64.b64encode(int.to_bytes(mt.extract_number(), 4, "big")).decode("ascii")
-        if(out == rn):
-            print("PRG Cracked!!! Seed: {}".format(i))
-            return
+    start = t-125 # How far to go back before you start testing times
+    stop = t-10 # When to stop testing times
+    found = False
+    while(found == False):
+        for i in range(start, stop):
+            mt = MersenneTwister.MersenneTwister(i)
+            out = base64.b64encode(int.to_bytes(mt.extract_number(), 4, "big")).decode("ascii")
+            if(out == rn):
+                print("PRG Cracked!!! Seed: {}".format(i))
+                return
+        # Set new values of start and stop, going back two minutes at a time
+        stop = start-1
+        start = start-121
     print("No seed found... D;")
 
 def main():
